06df33471e81a0a2f7b2b1578e538ff38cdbf489
Ori <orilevits@gmail.com>
2022-12-05 14:36:40 +0200

    added getting all games from all leagues aswell

--- main.py
+++ main.py
@@ -3,20 +3,15 @@ import soup_manager
 from tqdm import tqdm
 import helper
 
-league_table = urlopen('https://www.iva.org.il/boards.asp')
+LEAGUE_TABLE = urlopen('https://www.iva.org.il/boards.asp')
 
-all_leagues_links = soup_manager.get_leagues(league_table)
+all_leagues_links = soup_manager.get_leagues(LEAGUE_TABLE)
 all_teams = []
 all_games = []
 
-for link in tqdm(all_leagues_links, bar_format='{l_bar}{bar:100}', desc="Gathering information", leave=False,
-                 disable=False):
+for link in tqdm(all_leagues_links, bar_format='{l_bar}{bar:100}', desc="Gathering information", leave=False, disable=False):
     league_title = soup_manager.get_title(urlopen(link))
     soup_manager.get_teams(urlopen(link), all_teams)
     helper.get_games(link, league_title, all_games)
 
-helper.print_teams(all_teams)
-# helper.print_games(all_games[0]["games"],all_games[0]["title"])
 
-for _ in range(len(all_games)):
-    helper.print_games(all_games[_]["games"], all_games[_]["title"])
